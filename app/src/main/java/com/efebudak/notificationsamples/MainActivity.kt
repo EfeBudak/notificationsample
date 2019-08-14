955c02a7e931bc1cbf5ec13a28a808e42b03ca00
Efe Budak <efe.budak@fleetmatics.com>
2019-08-14 15:12:37 +0100

    Basic notification creations

--- app/src/main/java/com/efebudak/notificationsamples/MainActivity.kt
+++ app/src/main/java/com/efebudak/notificationsamples/MainActivity.kt
@@ -1,12 +1,73 @@
 package com.efebudak.notificationsamples
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import kotlinx.android.synthetic.main.activity_main.*
+
+private const val CHANNEL_NAME = "Sample Channel Name"
+private const val CHANNEL_DESCRIPTION = "Sample Channel Description"
+private const val CHANNEL_ID = "Channel Id (but as String)"
+
+private const val BASIC_NOTIFICATION_ID = 11
+private const val NOTIFICATION_WITH_ACTION_ID = 12
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        createNotificationChannel()
+
+        buttonBasic.setOnClickListener {
+            val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle("Basic Notification Title")
+                .setContentText("Basic Notification Text")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+            NotificationManagerCompat.from(this).notify(BASIC_NOTIFICATION_ID, builder.build())
+        }
+
+        buttonWithAction.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            }
+            val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+            val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle("Notification with Action Title")
+                .setContentText("Notification with Action Text")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+
+            NotificationManagerCompat.from(this)
+                .notify(NOTIFICATION_WITH_ACTION_ID, builder.build())
+        }
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel =
+                NotificationChannel(CHANNEL_ID, CHANNEL_NAME, importance).apply {
+                    description = CHANNEL_DESCRIPTION
+                }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 }
